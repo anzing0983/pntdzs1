f2e26418e6ab0e1d368f6f8e9bb2cec46787b7dd
Nguyễn Ngọc Huy Tâm <108131520+anzing0983@users.noreply.github.com>
2023-03-06 21:24:03 +0700

    Add files via upload

---
+++ ttmxh/public/js/config.js
@@ -0,0 +1,59 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'Pham Viet Tien',
+        'favicon' : 'https://i.imgur.com/c3bA3zR.jpg',
+        'notice': 'Pham Viet Tien DTRAI S1'
+    },
+    'avatar-image' : 'https://i.imgur.com/c3bA3zR.jpg',
+    'name' : 'Phạm Việt Tiến ', 
+    'is_tick' : true,
+    'skills' : [
+        'Student ',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'https://i.imgur.com/c3bA3zR.jpg',
+            'content' : 'CONTACT ADMIN',
+            'author' : 'Phạm Việt Tiến',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'wallet.png',
+            'title' : 'Thông Tin Thanh Toán', 
+            'show-value' : 'Phạm Việt Tiến',
+            'value' : 'https://pvt-bank.blogspot.com/',
+            'type' : 0,
+        },
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'Facebook', 
+            'show-value' : '@phamviettien.Info' ,
+            'value' : 'https://www.facebook.com/100086063431708',
+            'type' : 0,
+        },
+        {
+            'image' : 'zalo.jpg',
+            'title' : 'Zalo', 
+            'show-value' : 'Phạm Việt Tiến',
+            'value' : 'https://zalo.me/0918444317',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Hotline', 
+            'show-value' : 'xxxx xxx xxx',
+            'value' : 'tel:xxxx xxx xxx',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail', 
+            'show-value' : 'phamviettien030609@gmail.com',
+            'value' : 'mailto:phamviettien030609@gmail.com',
+            'type' : 0,
+        },
+    ]
+};
+
